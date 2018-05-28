3141263ddcc154d86fd1dbc28254fc3faf707360
tyfecho <32788268+tyfecho@users.noreply.github.com>
2018-05-28 18:05:59 +0800

    280518

--- Test2405/Controllers/LoginController.cs
+++ Test2405/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
+using Test2405.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,7 +12,7 @@ namespace Test2405.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(UserData.Users);
         }
 
         public string Welcome(string name, int numTimes = 1)
---
+++ Test2405/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test2405.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Domain { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserData
+    {
+        public static List<UserModel> Users { get; set; } = new List<UserModel>();
+    }
+}
