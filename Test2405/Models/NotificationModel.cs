c4cb5b9dce91e641e908a5cf83b45a42cfaf424c
tyfecho <32788268+tyfecho@users.noreply.github.com>
2018-06-18 18:15:32 +0800

    180618

--- Test2405/Models/NotificationModel.cs
+++ Test2405/Models/NotificationModel.cs
@@ -11,14 +11,21 @@ namespace Test2405.Models
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    
+
+    //  ID -> Posted ( Sent ) -> Platform -> Notifications -> Status -> PostedBy -> Action/Activity -> Expiry -> Priority
+    //  missing : Icon
+
     public class NotificationModel
     {
         public int NotificationID { get; set; }
         public string NotificationPosted { get; set; }
+        [DisplayName("Platform")]
         public string NotificationPlatform { get; set; }
+        [DisplayName("Notification Message")]
         public string NotificationMsg { get; set; }
+        [DisplayName("Status")]
         public string NotificationStatus { get; set; }
+        [DisplayName("Notification By")]
         public string NotificationBy { get; set; }
 
         //public string 
